9c5a76d73e7a3635fe0d21c036936338344e5ca2
ivanGorbache <ivan.gorbache@msmail.ariel.ac.il>
2024-02-05 19:28:39 +0200

    Forth commit

--- main.c
+++ main.c
@@ -1,9 +1,33 @@
 #include<stdio.h>
 #include "my_mat.h"
+#define MIN_ARGS 2
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     int mat[SIZE][SIZE];
-    inputMatrix(mat);
+	char c;
+    if(argc > MIN_ARGS)
+    {
+		FILE *f;
+		f = fopen(argv[1], "r"); 
+		do
+		{
+			c = (char)fgetc(f);
+			switch (c)
+			{
+				case 'A':
+					inputMatrix(mat);
+				case 'B':
+					pathExists(mat, (char)fgetc(f) - '0', (char)fgetc(f) - '0');
+				case 'C':
+					shortestPath(mat, (char)fgetc(f) - '0', (char)fgetc(f) - '0');
+				case 'D':
+					break;
+				default:
+					continue;
+			}
+		}while(c!=EOF);
+    }
+
     return 0;
 }
